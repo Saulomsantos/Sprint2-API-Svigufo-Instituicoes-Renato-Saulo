18066a844e18985dc72b6e25e63a0d7f26155951
Saulo Santos <saulo.macedoprojetos@gmail.com>
2019-04-07 17:21:12 -0300

    adicionado domains restantes

---
+++ senai.svigufo.webapi/Domains/EventoDomain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace senai.svigufo.webapi.Domains
+{
+    /// <summary>
+    /// Classe que faz referência à tabela Eventos
+    /// </summary>
+    public class EventoDomain
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o título do evento")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição do evento")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a data do evento")]
+        [DataType(DataType.Date)]
+        public DateTime DataEvento { get; set; }
+
+        [Required(ErrorMessage = "Informe se o acesso ao evento é livre")]
+        public bool AcessoLivre { get; set; }
+
+        [Required(ErrorMessage = "Informe o Id do Tipo de Evento")]
+        public int TipoEventoId { get; set; }
+        public TipoEventoDomain TipoEvento { get; set; }
+
+        [Required(ErrorMessage = "Informe o Id da insituição")]
+        public int InstituicaoId { get; set; }
+        public InstituicaoDomain Instituicao { get; set; }
+    }
+}
---
+++ senai.svigufo.webapi/Domains/UsuarioDomain.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai.svigufo.webapi.Domains
+{
+    /// <summary>
+    /// Classe responsável pela tabela Usuarios
+    /// </summary>
+    public class UsuarioDomain
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do usuário")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o e-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Informe o tipo do usuário")]
+        public string TipoUsuario { get; set; }
+    }
+}
