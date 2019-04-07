61049380a5f26b15db53a9bc10c58a4aa5ddd70b
Saulo Santos <saulo.macedoprojetos@gmail.com>
2019-04-07 18:40:06 -0300

    revisado os repositorios da instituicao e tipos eventos

--- senai.svigufo.webapi/Repositories/InstituicaoRepository.cs
+++ senai.svigufo.webapi/Repositories/InstituicaoRepository.cs
@@ -6,38 +6,42 @@ using senai.svigufo.webapi.Interfaces;
 
 namespace senai.svigufo.webapi.Repositories
 {
+    /// <summary>
+    /// Repositório da Instituição
+    /// </summary>
     public class InstituicaoRepository : IInstituicaoRepository
     {
-        private string StringConexao = "Data Source=.\\SqlExpress; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND; user id=sa; pwd=132";
-        //private string StringConexao = "Data Source=.\\SqlExpress; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND; integrated security=true";
+        // private string StringConexao = "Data Source=.\\SqlExpress; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND; user id=sa; pwd=132";
+        private string StringConexao = "Data Source=.\\SqlDeveloper; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND; integrated security=true";
+
         /// <summary>
         /// Lista todas as instituições
         /// </summary>
-        /// <returns>List<TiposEventoDomain></returns>
+        /// <returns>Retorra uma lista de instituições</returns>
         public List<InstituicaoDomain> Listar()
         {
-            //Cria a lista Instituicoes com todas as instituicoes para serem listadas
+            // Cria a lista ListaInstituicoes com todas as instituicoes para serem listadas
             List<InstituicaoDomain> ListaInstituicoes = new List<InstituicaoDomain>();
 
-            //Cria a conexao com o banco de dados
+            // Cria a conexao com o banco de dados
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
-                //Cria a query a ser comunicada com o banco de dados
+                // Cria a query a ser comunicada com o banco de dados
                 string QueryListar = "SELECT ID, NOME_FANTASIA, RAZAO_SOCIAL, CNPJ, LOGRADOURO, CEP, UF, CIDADE FROM INSTITUICOES";
 
-                //Abre a conexão com o banco de dados
+                // Abre a conexão com o banco de dados
                 con.Open();
 
-                //Percorre a lista através de um SqlDataReader
+                // Percorre a lista através de um SqlDataReader
                 SqlDataReader sdr;
 
-                //Cria um comando passando a query e a conexão
-                using (SqlCommand cmd = new SqlCommand(QueryListar,con))
+                // Cria um comando passando a query e a conexão
+                using (SqlCommand cmd = new SqlCommand(QueryListar, con))
                 {
-                    //Executa a query QueryListar através do comando cmd
+                    // Executa a query QueryListar através do comando cmd
                     sdr = cmd.ExecuteReader();
 
-                    //Percorrer os dados
+                    // Percorrer os dados
                     while (sdr.Read())
                     {
                         InstituicaoDomain instituicao = new InstituicaoDomain
@@ -50,9 +54,9 @@ namespace senai.svigufo.webapi.Repositories
                             CEP = sdr["CEP"].ToString(),
                             UF = sdr["UF"].ToString(),
                             Cidade = sdr["CIDADE"].ToString()
-                            //É necessário converter os campos recebidos pelo BD para string mesmo
-                            //que já estejam no BD como string, char ou varchar
-                            //pois no C# é recebido como objeto, não como variável
+                            // É necessário converter os campos recebidos pelo BD para string mesmo
+                            // que já estejam no BD como string, char ou varchar
+                            // pois no C# é recebido como objeto, não como variável
                         };
 
                         ListaInstituicoes.Add(instituicao);
@@ -67,35 +71,36 @@ namespace senai.svigufo.webapi.Repositories
         /// <summary>
         /// Grava uma nova instituição
         /// </summary>
-        /// <param name="Instituicao"></param>
+        /// <param name="Instituicao">Objeto instituicao</param>
         public void Gravar(InstituicaoDomain instituicao)
         {
-            // Declara a conexão passando a string
+            // Declara a conexão passando a string de conexão
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
-                // Declara a query passando o valor como parâmetro
+                // Declara a query passando o valor
                 string QueryGravar = "INSERT INTO INSTITUICOES (NOME_FANTASIA,RAZAO_SOCIAL,CNPJ,LOGRADOURO,CEP,UF,CIDADE) VALUES (@NOME_FANTASIA,@RAZAO_SOCIAL,@CNPJ,@LOGRADOURO,@CEP,@UF,@CIDADE)";
 
                 // Declara o command passando a query e a conexão
-                SqlCommand cmd = new SqlCommand(QueryGravar, con);
+                using (SqlCommand cmd = new SqlCommand(QueryGravar, con))
+                {
+                    // Passa o valor do parâmetro
+                    cmd.Parameters.AddWithValue("@NOME_FANTASIA", instituicao.NomeFantasia);
+                    cmd.Parameters.AddWithValue("@RAZAO_SOCIAL", instituicao.RazaoSocial);
+                    cmd.Parameters.AddWithValue("@CNPJ", instituicao.CNPJ);
+                    cmd.Parameters.AddWithValue("@LOGRADOURO", instituicao.Logradouro);
+                    cmd.Parameters.AddWithValue("@CEP", instituicao.CEP);
+                    cmd.Parameters.AddWithValue("@UF", instituicao.UF);
+                    cmd.Parameters.AddWithValue("@CIDADE", instituicao.Cidade);
 
-                //Passa o valor do parâmetro
-                cmd.Parameters.AddWithValue("@NOME_FANTASIA", instituicao.NomeFantasia);
-                cmd.Parameters.AddWithValue("@RAZAO_SOCIAL", instituicao.RazaoSocial);
-                cmd.Parameters.AddWithValue("@CNPJ", instituicao.CNPJ);
-                cmd.Parameters.AddWithValue("@LOGRADOURO", instituicao.Logradouro);
-                cmd.Parameters.AddWithValue("@CEP", instituicao.CEP);
-                cmd.Parameters.AddWithValue("@UF", instituicao.UF);
-                cmd.Parameters.AddWithValue("@CIDADE", instituicao.Cidade);
+                    //Abre a conexão
+                    con.Open();
 
-                //Abre a conexão
-                con.Open();
-
-                //Executa o comando
-                cmd.ExecuteNonQuery();
+                    //Executa o comando
+                    cmd.ExecuteNonQuery();
+                }
             }
         }
-        
+
         /// <summary>
         /// Edita uma instituição
         /// </summary>
@@ -103,101 +108,119 @@ namespace senai.svigufo.webapi.Repositories
         /// <param name="Instituicao">Objeto instituicao com os novos dados</param>
         public void Editar(int id, InstituicaoDomain Instituicao)
         {
-            // Declara a conexão passando a string
+            // Declara a conexão passando a string de conexão
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
-                // Declara a query passando a string e a conexão
+                // Declara a query passando o valor
                 string QueryEditar = "UPDATE INSTITUICOES SET NOME_FANTASIA = @NOME_FANTASIA," +
                     "RAZAO_SOCIAL = @RAZAO_SOCIAL," +
-                    "CNPJ = @CNPJ," + 
+                    "CNPJ = @CNPJ," +
                     "LOGRADOURO = @LOGRADOURO," +
                     "CEP = @CEP," +
                     "UF = @UF," +
                     "CIDADE = @CIDADE WHERE ID = @ID";
 
                 // Declara o comando passando a query e a conexão
-                SqlCommand cmd = new SqlCommand(QueryEditar, con);
+                using (SqlCommand cmd = new SqlCommand(QueryEditar, con))
+                {
+                    // Passa o valor do parâmetro
+                    cmd.Parameters.AddWithValue("@NOME_FANTASIA", Instituicao.NomeFantasia);
+                    cmd.Parameters.AddWithValue("@ID", Instituicao.Id);
+                    cmd.Parameters.AddWithValue("@RAZAO_SOCIAL", Instituicao.RazaoSocial);
+                    cmd.Parameters.AddWithValue("@CNPJ", Instituicao.CNPJ);
+                    cmd.Parameters.AddWithValue("@LOGRADOURO", Instituicao.Logradouro);
+                    cmd.Parameters.AddWithValue("@CEP", Instituicao.CEP);
+                    cmd.Parameters.AddWithValue("@UF", Instituicao.UF);
+                    cmd.Parameters.AddWithValue("@CIDADE", Instituicao.Cidade);
 
-                // Passa o valor do parâmetro
-                cmd.Parameters.AddWithValue("@NOME_FANTASIA", Instituicao.NomeFantasia);
-                cmd.Parameters.AddWithValue("@ID", Instituicao.Id);
-                cmd.Parameters.AddWithValue("@RAZAO_SOCIAL", Instituicao.RazaoSocial);
-                cmd.Parameters.AddWithValue("@CNPJ", Instituicao.CNPJ);
-                cmd.Parameters.AddWithValue("@LOGRADOURO", Instituicao.Logradouro);
-                cmd.Parameters.AddWithValue("@CEP", Instituicao.CEP);
-                cmd.Parameters.AddWithValue("@UF", Instituicao.UF);
-                cmd.Parameters.AddWithValue("@CIDADE", Instituicao.Cidade);
+                    // Abre a conexão
+                    con.Open();
 
-                // Abre a conexão
-                con.Open();
-
-                // Executa o comando 
-                cmd.ExecuteNonQuery();
+                    // Executa o comando 
+                    cmd.ExecuteNonQuery();
+                }
             }
         }
 
+        /// <summary>
+        /// Deleta uma instituição
+        /// </summary>
+        /// <param name="id">Id da instituição a ser deletada</param>
         public void Deletar(int id)
         {
+            // Declara a conexão com o banco de dados passando a string de conexao
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
+                // Declara a query passando o valor
                 string QueryDeletar = "DELETE FROM INSTITUICOES WHERE ID = @ID";
 
-                SqlCommand cmd = new SqlCommand(QueryDeletar, con);
+                // Declara o comando passando a query e a conexão
+                using (SqlCommand cmd = new SqlCommand(QueryDeletar, con))
+                {
+                    // Passa o valor do parâmetro
+                    cmd.Parameters.AddWithValue("@ID", id);
 
-                cmd.Parameters.AddWithValue("@ID", id);
+                    // Abre a conexão com o banco de dados
+                    con.Open();
 
-                con.Open();
-
-                cmd.ExecuteNonQuery();
+                    // Executa o comando
+                    cmd.ExecuteNonQuery();
+                }
             }
         }
 
+        /// <summary>
+        /// Busca uma instituição pelo Id
+        /// </summary>
+        /// <param name="id">Id da instituição</param>
+        /// <returns>Retorna uma instituição</returns>
         public InstituicaoDomain GetById(int id)
         {
-            //Cria a conexao com o banco de dados
+            // Cria a conexao com o banco de dados
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
-                //Cria a query a ser comunicada com o banco de dados
+                // Cria a query a ser comunicada com o banco de dados
                 string QueryBuscar = "SELECT ID, NOME_FANTASIA, RAZAO_SOCIAL, CNPJ, LOGRADOURO, CEP, UF, CIDADE FROM INSTITUICOES";
 
-                //Abre a conexão com o banco de dados
+                // Abre a conexão com o banco de dados
                 con.Open();
 
-                //Percorre a lista através de um SqlDataReader
+                // Percorre a lista através de um SqlDataReader
                 SqlDataReader sdr;
 
-                //Cria um comando passando a query e a conexão
+                // Cria um comando passando a query e a conexão
                 using (SqlCommand cmd = new SqlCommand(QueryBuscar, con))
                 {
-                    //Executa a query QueryListar através do comando cmd
-                    sdr = cmd.ExecuteReader();
+                    // Executa a query QueryListar através do comando cmd
+                    sdr = cmd.ExecuteReader();                    
 
-                    //Percorrer os dados
-                    while (sdr.Read())
+                    if (sdr.HasRows)
                     {
-                        InstituicaoDomain instituicao = new InstituicaoDomain
+                        // Percorrer os dados
+                        while (sdr.Read())
                         {
-                            Id = Convert.ToInt32(sdr["ID"]),
-                            NomeFantasia = sdr["NOME_FANTASIA"].ToString(),
-                            RazaoSocial = sdr["RAZAO_SOCIAL"].ToString(),
-                            CNPJ = sdr["CNPJ"].ToString(),
-                            Logradouro = sdr["LOGRADOURO"].ToString(),
-                            CEP = sdr["CEP"].ToString(),
-                            UF = sdr["UF"].ToString(),
-                            Cidade = sdr["CIDADE"].ToString()
-                            //É necessário converter os campos recebidos pelo BD para string mesmo
-                            //que já estejam no BD como string, char ou varchar
-                            //pois no C# é recebido como objeto, não como variável
-                        };
+                            InstituicaoDomain instituicao = new InstituicaoDomain
+                            {
+                                Id = Convert.ToInt32(sdr["ID"]),
+                                NomeFantasia = sdr["NOME_FANTASIA"].ToString(),
+                                RazaoSocial = sdr["RAZAO_SOCIAL"].ToString(),
+                                CNPJ = sdr["CNPJ"].ToString(),
+                                Logradouro = sdr["LOGRADOURO"].ToString(),
+                                CEP = sdr["CEP"].ToString(),
+                                UF = sdr["UF"].ToString(),
+                                Cidade = sdr["CIDADE"].ToString()
+                                // É necessário converter os campos recebidos pelo BD para string mesmo
+                                // que já estejam no BD como string, char ou varchar
+                                // pois no C# é recebido como objeto, não como variável
+                            };
 
-                        if (sdr.HasRows)
-                        {
                             return instituicao;
                         }
-
-                        return null;
-
                     }
+
+                    return null;
+                }
             }
+        }
     }
 }
--- senai.svigufo.webapi/Repositories/TipoEventoRepository.cs
+++ senai.svigufo.webapi/Repositories/TipoEventoRepository.cs
@@ -6,24 +6,38 @@ using senai.svigufo.webapi.Interfaces;
 
 namespace senai.svigufo.webapi.Repositories
 {
+    /// <summary>
+    /// Repositório dos tipos eventos
+    /// </summary>
     public class TipoEventoRepository : ITipoEventoRepository
     {
-        private string StringConexao = "Data Source=.\\SqlExpress; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND;user id=sa; pwd=132";
+        // Na dúvida, procurar no site connection string qual a string de conexão do banco utilizado
+        // private string StringConexao = "Data Source=.\\SqlExpress; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND;user id=sa; pwd=132";
+        private string StringConexao = "Data Source=.\\SqlDeveloper; initial catalog=SENAI_SVIGUFO_MANHA_BACKEND; integrated security=true";
 
+        /// <summary>
+        /// Altera um tipo de evento
+        /// </summary>
+        /// <param name="tipoEvento">Recebe um objeto tipoEvento</param>
         public void Alterar(TipoEventoDomain tipoEvento)
         {
+            // Define a conexão passando a string
             using(SqlConnection con = new SqlConnection(StringConexao))
             {
+                // Define a query que será comunicada com o banco de dados
                 string Query = "UPDATE TIPOS_EVENTOS SET TITULO = @TITULO WHERE ID = @ID";
 
+                // Define o comando passando a query e a conexão
                 SqlCommand cmd = new SqlCommand(Query, con);
 
+                // Passa o valor dos parâmetros
                 cmd.Parameters.AddWithValue("@TITULO", tipoEvento.Nome);
-
                 cmd.Parameters.AddWithValue("@ID", tipoEvento.Id);
 
+                // Abre a conexão com o banco
                 con.Open();
-
+                
+                // Executa o comando
                 cmd.ExecuteNonQuery();
             }
         }
@@ -34,44 +48,63 @@ namespace senai.svigufo.webapi.Repositories
         /// <param name="tipoEvento">TipoEventoDomain</param>
         public void Cadastrar(TipoEventoDomain tipoEvento)
         {
+            // Declara a conexão passando a string de conexão
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
                 // Não usar dessa forma pois pode causar o efeito Joana D'arc
-                //string QuerySerExecutada = "INSERT INTO TIPOS_EVENTOS(TITULO) VALUES ('" + tipoEvento.Nome + "')";
-                //Declara a query passando o valor como parametro
+                // string QuerySerExecutada = "INSERT INTO TIPOS_EVENTOS(TITULO) VALUES ('" + tipoEvento.Nome + "')";
+                
+                // Declara a query passando o valor como parametro
                 string QuerySerExecutada = "INSERT INTO TIPOS_EVENTOS(TITULO) VALUES (@TITULO)";
 
-                //Declara o command passando a query e a conexão
+                // Declara o command passando a query e a conexão
                 SqlCommand cmd = new SqlCommand(QuerySerExecutada, con);
 
+                // Passa o valor do parâmetro
                 cmd.Parameters.AddWithValue("@TITULO", tipoEvento.Nome);
 
+                // Abre a conexão com o banco
                 con.Open();
 
+                // Executa o comando
                 cmd.ExecuteNonQuery();
 
             }
         }
 
+        /// <summary>
+        /// Deleta um tipo de evento
+        /// </summary>
+        /// <param name="id">Id do tipo de evento a ser deletado</param>
         public void Deletar(int id)
         {
+            // Declara a conexão passando a string de conexão
             using (SqlConnection con = new SqlConnection(StringConexao))
             {
+                // Declara a query a ser executada
                 string Query = "DELETE FROM TIPOS_EVENTOS WHERE ID = @ID";
 
+                // Declara o comando passando a query e a conexão
                 SqlCommand cmd = new SqlCommand(Query, con);
 
+                // Passa o valor do parâmetro
                 cmd.Parameters.AddWithValue("@ID", id);
 
+                // Abre a conexão com o banco
                 con.Open();
 
+                // Executa o comando
                 cmd.ExecuteNonQuery();
             }
         }
 
-        // Na dúvida, procurar no site connection string qual a string de conexão do banco utilizado
+        /// <summary>
+        /// Lista os tipos de eventos
+        /// </summary>
+        /// <returns>Retorna uma lista com os tipos de eventos</returns>
         public List<TipoEventoDomain> Listar()
         {
+            // Cria uma lista tiposEventos onde serão armazenados os dados
             List<TipoEventoDomain> tiposEventos = new List<TipoEventoDomain>();
 
             // Declaro a SqlConnection passando a string de conexão
@@ -101,11 +134,13 @@ namespace senai.svigufo.webapi.Repositories
                             Nome = rdr["TITULO"].ToString()
                         };
 
+                        // Adiciona um tipo de evento à lista
                         tiposEventos.Add(tipoEvento);
                     }
                 }
             }
 
+            // Retorna a lista
             return tiposEventos;
         }
     }
